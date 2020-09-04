b186949988f690d6c42dc8ad72dc8e7335d9e2aa
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-09-04 18:49:04 +0900

    barber

---
+++ app/controllers/barber_controller.rb
@@ -0,0 +1,5 @@
+class BarberController < ApplicationController
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "home#index"
   resources :profiles, only: [:index]
+  resources :barber, only: [:index]
  
 end
