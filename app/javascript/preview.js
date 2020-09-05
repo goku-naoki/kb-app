36ad255348ab50d684355e96292ea921ee126248
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-09-06 00:33:42 +0900

    まだ

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
   // const imageInputs=document.getElementBy('item_images')
-  
+  let imageInputs=[];
   const preview=()=>{
-    const imageInputs=Array.from(document.getElementsByClassName('image-field'));
+    imageInputs=Array.from(document.getElementsByClassName('image-field'));
+    const imageLength=Array.from(document.getElementsByClassName('create-item-form-image-preview-box')).length
     console.log(imageInputs)
   
     const createImage=(src)=>{
@@ -16,24 +17,35 @@ document.addEventListener('DOMContentLoaded',()=>{
     }
     const createInput=(index)=>{
       const imageInput=`
-      <input name="item[images][]" data-index="${index}" class="image-field" type="file" id="item_images">
+      <input name="item[images][]" data-index="${index}" class="image-field" type="file" id="image-${index}-input" style="display:none;">
       `
       return imageInput
     }
 
     
+
+    
+    
     imageInputs.forEach((imageInput)=>{
 
-      // const icon=document.getElementById('image-icon')
-      // icon.addEventListener('click',()=>{
-      // const target =imageInputs[imageInputs.length-1]
-      // console.log(target)
-      // const targetNum=target.getAttribute('data-index')
-      // const newtarget=document.querySelector(`.image-field[data-index="${targetNum}"`)
-      // console.log(newtarget)
-      // newtarget.click()
-      //    })ここ改善
+      const icon=document.getElementById('image-icon')
+      icon.addEventListener('click',()=>{
+      const target =imageInputs[imageInputs.length-1]
+      console.log('hoge')
+      const targetIndex=Number(target.getAttribute('data-index'))
+      const newtarget=document.getElementById(`image-${targetIndex}-input`)
+      console.log(imageLength)
+      if(newtarget==imageInput && imageLength!=3){
+        imageInput.click()
+        }
+       })
+
+       
       imageInput.addEventListener('change',(event)=>{
+        // if(imageInputs.length<4){ 
+       
+        console.log(imageLength)
+        if(imageLength<3){
         const index=Number(event.target.getAttribute('data-index'))
         console.log(index)
         const file=event.target.files[0]
@@ -41,7 +53,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         const imageBox=createImage(imageSrc);
         const previewBox=document.getElementById('image-preview')
         previewBox.insertAdjacentHTML('beforeend', imageBox);
-          if(imageInputs.length<3){  //ここでif文かけるといい感じ。上だと３枚目が出ない。
+          if(imageLength<2){  //ここでif文かけるといい感じ。上だと３枚目が出ない。
           const nextIndex=index + 1
           const imageInput=createInput(nextIndex)
           const inputBox=document.getElementById('input-box')
@@ -50,11 +62,15 @@ document.addEventListener('DOMContentLoaded',()=>{
           }else{
             return false
           }
+        }
+        // }
       })
     })
   }
 
-  
+ 
 
   preview()
+
+  
 })
