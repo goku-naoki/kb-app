a2340b50606fedf1b75e4253254a546d0f126fdb
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-09-06 14:58:13 +0900

    jsの修正必要大いにあり

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,75 +2,96 @@ document.addEventListener('DOMContentLoaded',()=>{
 
   // const imageInputs=document.getElementBy('item_images')
   let imageInputs=[];
+  let imageLength=0;
+  let editBtns=[]
   const preview=()=>{
     imageInputs=Array.from(document.getElementsByClassName('image-field'));
-    const imageLength=Array.from(document.getElementsByClassName('create-item-form-image-preview-box')).length
-    console.log(imageInputs)
+    imageLength=Array.from(document.getElementsByClassName('create-item-form-image-preview-box')).length;
+    editBtns=Array.from(document.getElementsByClassName('image-edit'))
+   
   
-    const createImage=(src)=>{
-      const imageBox=`
-        <div class="create-item-form-image-preview-box">
-          <img src="${src}">
-        </div>
-      `
-        return imageBox
-    }
-    const createInput=(index)=>{
-      const imageInput=`
-      <input name="item[images][]" data-index="${index}" class="image-field" type="file" id="image-${index}-input" style="display:none;">
-      `
-      return imageInput
-    }
-
     
+    const createImage=(src,index)=>{
+        const imageBox=`
+          <div class="create-item-form-image-preview-box" id="preview-${index}-box" data-index="${index}">
+            <div class="image-box">
+              <img id="image-${index}" src="${src}">
+            </div>
+          <p class="image-edit">edit</p>
+          </div>
+        `
+          return imageBox
+      }
+      
+      const createInput=(index)=>{
+        const imageInput=`
+        <input name="item[images][]" data-index="${index}" class="image-field" type="file" id="image-${index}-input" >
+        `
+        return imageInput
+      }
 
+      
+
+    // if(editBtns !=null){
+    //   editBtns.forEach((edit)=>{
+    //    edit.addEventListener('click',(event)=>{
+    //      const target =event.target.parentNode.getAttribute('data-index')
+    //      const newtarget=document.getElementById(`image-${target}-input`)
+    //      console.log(newtarget)
+    //       newtarget.click()
+    //          })
+    //      })
+    
+    //  }
+
+     const icon=document.getElementById('image-icon')
+        icon.addEventListener('click',()=>{
+          const target =imageInputs[imageInputs.length-1]
+          const targetIndex=Number(target.getAttribute('data-index'))
+          const newtarget=document.getElementById(`image-${targetIndex}-input`)
+          console.log(target)
+          if(imageLength!=3){
+            newtarget.click()
+            }
+        })
     
     
     imageInputs.forEach((imageInput)=>{
 
-      const icon=document.getElementById('image-icon')
-      icon.addEventListener('click',()=>{
-      const target =imageInputs[imageInputs.length-1]
-      console.log('hoge')
-      const targetIndex=Number(target.getAttribute('data-index'))
-      const newtarget=document.getElementById(`image-${targetIndex}-input`)
-      console.log(imageLength)
-      if(newtarget==imageInput && imageLength!=3){
-        imageInput.click()
-        }
-       })
-
-       
-      imageInput.addEventListener('change',(event)=>{
-        // if(imageInputs.length<4){ 
-       
-        console.log(imageLength)
-        if(imageLength<3){
-        const index=Number(event.target.getAttribute('data-index'))
-        console.log(index)
-        const file=event.target.files[0]
-        const imageSrc=URL.createObjectURL(file)
-        const imageBox=createImage(imageSrc);
-        const previewBox=document.getElementById('image-preview')
-        previewBox.insertAdjacentHTML('beforeend', imageBox);
-          if(imageLength<2){  //ここでif文かけるといい感じ。上だと３枚目が出ない。
-          const nextIndex=index + 1
-          const imageInput=createInput(nextIndex)
-          const inputBox=document.getElementById('input-box')
-          inputBox.insertAdjacentHTML('beforeend', imageInput);
+        
+        imageInput.addEventListener('change',(event)=>{
+      
+          const index=Number(event.target.getAttribute('data-index'))
+          const file=event.target.files[0]
+          const imageSrc=URL.createObjectURL(file)
+          const editItem=document.getElementById(`image-${index}`)
+         
+          // if(editItem!=null){
+          //   console.log(editItem)
+          //   editItem.setAttribute('src',imageSrc)
+          // }else{
+          const imageBox=createImage(imageSrc,index);
+          const previewBox=document.getElementById('image-preview')
+          previewBox.insertAdjacentHTML('beforeend', imageBox);
+            if(imageLength<2){  //ここでif文かけるといい感じ。上だと３枚目が出ない。
+              console.log(imageLength)
+              const nextIndex=index + 1
+              const imageInput=createInput(nextIndex)
+              const inputBox=document.getElementById('input-box')
+              inputBox.insertAdjacentHTML('beforeend', imageInput);
+              
+            }//むやみにretur false書けるとpreviewが読み込まれずうまく動かない
+            
+          // }
           preview()
-          }else{
-            return false
-          }
-        }
-        // }
-      })
+        })
     })
+  
   }
 
  
 
-  preview()
+ preview()
 
   
 })
