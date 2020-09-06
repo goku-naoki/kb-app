897e4cac10f1c9695fbc35d19c87b36f79295668
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-09-06 19:52:46 +0900

    プレヴュー完成！！！
    clickアクションに制限を書けるのが味噌だたt。

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,12 +1,40 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
   // const imageInputs=document.getElementBy('item_images')
-  let imageInputs=[];
+  let imageInputs=Array.from(document.getElementsByClassName('image-field'));
   let imageLength=0;
   let editBtns=[]
-  const preview=()=>{
-    imageInputs=Array.from(document.getElementsByClassName('image-field'));
-    imageLength=Array.from(document.getElementsByClassName('create-item-form-image-preview-box')).length;
+  let index=0
+  let imageSrc=""
+  const icon=document.getElementById('image-icon')
+  let newtarget=""
+  
+
+
+  function iconClick(){
+    icon.addEventListener('click',()=>{
+      //この二つをさらに読みこます
+      imageInputs=Array.from(document.getElementsByClassName('image-field'));
+      imageLength=Array.from(document.getElementsByClassName('create-item-form-image-preview-box')).length;
+      let target =imageInputs[imageInputs.length-1]
+      // const targetIndex=Number(target.getAttribute('data-index'))
+      let targetIndex=Number(target.getAttribute('data-index'))
+      newtarget=document.getElementById(`image-${targetIndex}-input`)
+      console.log(target)
+      if(imageLength!=3){
+        newtarget.click()
+        }
+      // event.currentTarget.removeEventListener(event.type, clickEvent);
+    },{
+      once: true  //イベント発火回数を制限しないと下のクリックと二回怒る
+    })
+  }
+ 
+  const preview=(imageSrc,index)=>{
+    // imageInputs=Array.from(document.getElementsByClassName('image-field'));
+    // imageLength=Array.from(document.getElementsByClassName('create-item-form-image-preview-box')).length;
+    //clickイベントで更新されるから必要ない上
+
     editBtns=Array.from(document.getElementsByClassName('image-edit'))
    
   
@@ -30,8 +58,19 @@ document.addEventListener('DOMContentLoaded',()=>{
         return imageInput
       }
 
-      
+      const imageBox=createImage(imageSrc,index);
+      const previewBox=document.getElementById('image-preview')
+          previewBox.insertAdjacentHTML('beforeend', imageBox);
+            if(imageLength<2){  //ここでif文かけるといい感じ。上だと３枚目が出ない。
+              console.log(imageLength)
+              const nextIndex=index + 1
+              const imageInput=createInput(nextIndex)
+              const inputBox=document.getElementById('input-box')
+              inputBox.insertAdjacentHTML('beforeend', imageInput);
+              
+            }//むやみにretur false書けるとpreviewが読み込まれずうまく動かない
 
+  
     // if(editBtns !=null){
     //   editBtns.forEach((edit)=>{
     //    edit.addEventListener('click',(event)=>{
@@ -44,54 +83,46 @@ document.addEventListener('DOMContentLoaded',()=>{
     
     //  }
 
-     const icon=document.getElementById('image-icon')
-        icon.addEventListener('click',()=>{
-          const target =imageInputs[imageInputs.length-1]
-          const targetIndex=Number(target.getAttribute('data-index'))
-          const newtarget=document.getElementById(`image-${targetIndex}-input`)
-          console.log(target)
-          if(imageLength!=3){
-            newtarget.click()
-            }
-        })
+    //  const icon=document.getElementById('image-icon')
+    //     icon.addEventListener('click',()=>{
+    //       const target =imageInputs[imageInputs.length-1]
+    //       const targetIndex=Number(target.getAttribute('data-index'))
+    //       const newtarget=document.getElementById(`image-${targetIndex}-input`)
+    //       console.log(target)
+    //       if(imageLength!=3){
+    //         newtarget.click()
+    //         }
+    //     })
     
     
-    imageInputs.forEach((imageInput)=>{
+    // imageInputs.forEach((imageInput)=>{
 
-        
-        imageInput.addEventListener('change',(event)=>{
-      
-          const index=Number(event.target.getAttribute('data-index'))
+       iconClick()
+       const nextTarget=document.getElementById(`image-${index + 1}-input`)
+       
+        nextTarget.addEventListener('change',(event)=>{
+          console.log(nextTarget)
+          index=Number(event.target.getAttribute('data-index'))
           const file=event.target.files[0]
-          const imageSrc=URL.createObjectURL(file)
+          imageSrc=URL.createObjectURL(file)
           const editItem=document.getElementById(`image-${index}`)
-         
-          // if(editItem!=null){
-          //   console.log(editItem)
-          //   editItem.setAttribute('src',imageSrc)
-          // }else{
-          const imageBox=createImage(imageSrc,index);
-          const previewBox=document.getElementById('image-preview')
-          previewBox.insertAdjacentHTML('beforeend', imageBox);
-            if(imageLength<2){  //ここでif文かけるといい感じ。上だと３枚目が出ない。
-              console.log(imageLength)
-              const nextIndex=index + 1
-              const imageInput=createInput(nextIndex)
-              const inputBox=document.getElementById('input-box')
-              inputBox.insertAdjacentHTML('beforeend', imageInput);
-              
-            }//むやみにretur false書けるとpreviewが読み込まれずうまく動かない
-            
-          // }
-          preview()
-        })
-    })
-  
+          preview(imageSrc,index)     
+        }) 
   }
 
+   
  
+  iconClick()
+  
+  const firstTarget=document.getElementById("image-0-input")
+  firstTarget.addEventListener('change',(event)=>{
+    const file=event.target.files[0]
+    imageSrc=URL.createObjectURL(file)
+    index=Number(event.target.getAttribute('data-index'))
+    console.log('change!!')
+    preview(imageSrc,index);
+  });
 
- preview()
 
   
 })
