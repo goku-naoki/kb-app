6be2609adca7cf6cbab11ebf501d38e5c5b0ad8e
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-09-07 13:57:16 +0900

    完璧に終了

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -6,110 +6,71 @@ document.addEventListener('DOMContentLoaded',()=>{
   const icon=document.getElementById('image-icon')
   
   
-    function changeInput(targetInput){ 
-
-      if(targetInput.addEventListener('change',(event)=>{
-        const thisEvent=event
-        prepareImageField(thisEvent)
-        })){
-          console.log("hoge")
-        }else{
-          iconClick()
-        }
-    }
-
-    function iconClick(){
-      icon.addEventListener('click',()=>{
+    function triggerInput(){
         imageInputs=Array.from(document.getElementsByClassName('image-field'));
         imageLength=Array.from(document.getElementsByClassName('create-item-form-image-preview-box')).length;
-        let target =imageInputs[imageInputs.length-1]
-        console.log(imageLength)
-          if(imageLength!=3){
-            console.log(target)
-            // target.click()
-            changeInput(target)
-          } 
-        },{
-          once: true  //イベント発火回数を制限しないと下のクリックと二回怒る
-        })
+        if(imageLength!=3){
+          imageInputs.forEach((input)=>{
+            input.addEventListener('change',(event)=>{
+              const index=Number(event.target.getAttribute('data-index'))
+              const file=event.target.files[0]
+              const imageSrc=URL.createObjectURL(file)
+              preview(imageSrc,index)
+            })
+          })
+        }
       } 
-    function editClick(){
-      window.addEventListener('click',(event)=>{
-        if(event.srcElement.classList.contains('image-edit')){
-          console.log(event)
-          const editIndex=Number(event.target.parentNode.getAttribute('data-index'))
-          const editTarget=document.getElementById(`image-${editIndex}-input`)
-          editTarget.click()
-          changeInput(editTarget)
-        }
-      },{
-  
-      })
-    }
-
-    function prepareImageField(event){
-      const index=Number(event.target.getAttribute('data-index'))
-      if(event.target.files.length==0){
-        // editClick()
-      }else{
-        const file=event.target.files[0]
-        const imageSrc=URL.createObjectURL(file)
-        preview(imageSrc,index)
-      }
-      }
-
-
-    const preview=(imageSrc,index)=>{
-
-      
-      const imagePreview=document.getElementById(`image-${index}`)
-      if(imagePreview != null){
-        imagePreview.setAttribute('src',imageSrc)
-      }else{//既存のイメージがなかったら、下でimageとinput作成
-
-        const createImage=(src,index)=>{ //imageの箱
-            const imageBox=`
-              <div class="create-item-form-image-preview-box" id="preview-${index}-box" data-index="${index}">
-                <div class="image-box">
-                  <img id="image-${index}" src="${src}">
-                </div>
+    
+      const createImage=(src,index)=>{ //imageの箱
+        const imageBox=`
+          <div class="create-item-form-image-preview-box" id="preview-${index}-box" data-index="${index}">
+            <div class="image-box">
+              <img id="image-${index}" src="${src}">
+            </div>
+            <div class="create-item-form-image-preview-option">
               <label for="image-${index}-input">
-                <p class="image-edit" >edit</p>
+                <p class="image-edit">edit</p>
               </label>
-              </div>
-            `
-              return imageBox
-          }
+              <p class="image-delete">削除</p>
+            </div>
+          </div>
+        `
+          return imageBox
+      }
 
-          const createInput=(index)=>{  //input要素
-            const imageInput=`
-           
-            <input name="item[images][]" data-index="${index}" class="image-field" type="file" id="image-${index}-input" >
-            `
-            return imageInput
-            }
-
-          const imageBox=createImage(imageSrc,index);
-          const previewBox=document.getElementById('image-preview')
-          previewBox.insertAdjacentHTML('beforeend', imageBox);
-          const nextIndex=index + 1
-          const imageLabel=document.getElementById('image-label')
-          imageLabel.setAttribute('for',`image-${nextIndex}-input`)
-            if(imageLength<2){  //ここでif文かけるといい感じ。上だと３枚目が出ない。
-              console.log(imageLength)
-           
-              const imageInput=createInput(nextIndex)
-              const inputBox=document.getElementById('input-box')
-              inputBox.insertAdjacentHTML('beforeend', imageInput);
-              
-            }
-          iconClick()
+      const createInput=(index)=>{  //input要素
+        const imageInput=`
+          <input name="item[images][]" data-index="${index}" class="image-field" type="file" id="image-${index}-input" >
+        `
+        return imageInput
         }
-    }
 
+      const preview=(imageSrc,index)=>{
+
+        const imagePreview=document.getElementById(`image-${index}`)
+        if(imagePreview != null){
+          imagePreview.setAttribute('src',imageSrc)
+        }
+        else{//既存のイメージがなかったら、下でimageとinput作成
+            const imageBox=createImage(imageSrc,index);
+            const previewBox=document.getElementById('image-preview')
+            previewBox.insertAdjacentHTML('beforeend', imageBox);
+            const nextIndex=index + 1
+            const imageLabel=document.getElementById('image-label')
+            imageLabel.setAttribute('for',`image-${nextIndex}-input`)
+              if(imageLength<2){  //ここでif文かけるといい感じ。上だと３枚目が出ない。
+                console.log(imageLength)
+            
+                const imageInput=createInput(nextIndex)
+                const inputBox=document.getElementById('input-box')
+                inputBox.insertAdjacentHTML('beforeend', imageInput);
+                
+              }      
+          }
+        triggerInput()  //以前までは一個上のif文内にあった
+    }
     //全ての始まりはここ
 
-    iconClick()
-   
-    // editClick()
+    triggerInput()
+ 
 })
